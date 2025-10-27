d820ea507f6d758546f9e3a89806731681c1df88
ThreeeJ <2171266@hansung.ac.kr>
2025-10-27 13:10:18 +0900

    refactor: Swagger 어노테이션을 UserApi 인터페이스로 분리

--- src/main/java/org/likelion/_thon/silver_navi/domain/user/web/controller/UserController.java
+++ src/main/java/org/likelion/_thon/silver_navi/domain/user/web/controller/UserController.java
@@ -1,17 +1,9 @@
 package org.likelion._thon.silver_navi.domain.user.web.controller;
 
-
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.likelion._thon.silver_navi.domain.user.service.UserService;
 import org.likelion._thon.silver_navi.domain.user.web.dto.SignUpReq;
-import org.likelion._thon.silver_navi.global.response.ErrorResponse;
 import org.likelion._thon.silver_navi.global.response.SuccessResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,37 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Tag(name = "사용자 API", description = "사용자 관련 API")
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-public class UserController {
+public class UserController implements UserApi {
     private final UserService userService;
 
-    //회원 가입
-    @ApiResponses({
-            @ApiResponse(responseCode = "201", description = "가입 성공",
-                    content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
-            @ApiResponse(
-                    responseCode = "409",
-                    description = "아이디(전화번호) 중복",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = ErrorResponse.class),
-                            examples = @ExampleObject(
-                                    value = """
-                                    {
-                                      "isSuccess": false,
-                                      "timestamp": "2025-10-27 10:10:21",
-                                      "code": "SIGNUP_409_1",
-                                      "httpStatus": 409,
-                                      "message": "이미 존재하는 아이디입니다.",
-                                      "data": null
-                                    }
-                                    """
-                            )
-                    )
-            )
-    })
+    // 회원 가입
+    @Override
     @PostMapping("/signup")
     public ResponseEntity<SuccessResponse<?>> signup(@RequestBody @Valid SignUpReq signupReq) {
         userService.signUp(signupReq);
