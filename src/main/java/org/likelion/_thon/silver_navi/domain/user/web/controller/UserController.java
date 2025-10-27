82d759268a520fac4ae924d703b648b053a4a952
ThreeeJ <2171266@hansung.ac.kr>
2025-10-27 10:33:17 +0900

    docs: 회원가입 API 스웨거 응답 상세화 예시 추가

--- src/main/java/org/likelion/_thon/silver_navi/domain/user/web/controller/UserController.java
+++ src/main/java/org/likelion/_thon/silver_navi/domain/user/web/controller/UserController.java
@@ -1,10 +1,17 @@
 package org.likelion._thon.silver_navi.domain.user.web.controller;
 
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.likelion._thon.silver_navi.domain.user.service.UserService;
 import org.likelion._thon.silver_navi.domain.user.web.dto.SignUpReq;
+import org.likelion._thon.silver_navi.global.response.ErrorResponse;
 import org.likelion._thon.silver_navi.global.response.SuccessResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +21,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "사용자 API", description = "사용자 관련 API")
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
     //회원 가입
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "가입 성공",
+                    content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "아이디(전화번호) 중복",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class),
+                            examples = @ExampleObject(
+                                    value = """
+                                    {
+                                      "isSuccess": false,
+                                      "timestamp": "2025-10-27 10:10:21",
+                                      "code": "SIGNUP_409_1",
+                                      "httpStatus": 409,
+                                      "message": "이미 존재하는 아이디입니다.",
+                                      "data": null
+                                    }
+                                    """
+                            )
+                    )
+            )
+    })
     @PostMapping("/signup")
     public ResponseEntity<SuccessResponse<?>> signup(@RequestBody @Valid SignUpReq signupReq) {
         userService.signUp(signupReq);
