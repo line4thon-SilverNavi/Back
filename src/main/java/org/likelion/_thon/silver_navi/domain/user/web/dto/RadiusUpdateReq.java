7f03bab554b3cfa5c5c54b5445f233892cdbe647
Seong Jin <oroi2009@gmail.com>
2025-11-02 17:11:07 +0900

    refactor : validation 중복 메시지 제거

--- src/main/java/org/likelion/_thon/silver_navi/domain/user/web/dto/RadiusUpdateReq.java
+++ src/main/java/org/likelion/_thon/silver_navi/domain/user/web/dto/RadiusUpdateReq.java
@@ -4,11 +4,11 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Range;
 
 @Getter
 public class RadiusUpdateReq {
     @NotNull(message = "거리 반경은 필수 입력 항목입니다.")
-    @Min(value = 3, message = "검색 반경은 3~7km 이어야 합니다.")
-    @Max(value = 7, message = "검색 반경은 3~7km 이어야 합니다.")
+    @Range(min = 3, max = 7, message = "검색 반경은 3~7km 이어야 합니다.")
     private Integer searchRadius;
 }
