00ca0f4244e9c9ee350420d671c04039d0c89c39
ThreeeJ <2171266@hansung.ac.kr>
2025-11-03 18:35:19 +0900

    refactor: RelationRole에서 부모(PARENT) 역할 제거

--- src/main/java/org/likelion/_thon/silver_navi/domain/user/entity/enums/RelationRole.java
+++ src/main/java/org/likelion/_thon/silver_navi/domain/user/entity/enums/RelationRole.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum RelationRole {
     SELF("self"),       // 본인
-    PARENT("parent"),   // 부모
     SPOUSE("spouse"),   // 배우자
     CHILD("child"),     // 자녀
     SIBLING("sibling"); // 형제/자매
