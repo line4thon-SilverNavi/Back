620ac912d22095da6e78e4919ce612617eebbfe3
Seong Jin <oroi2009@gmail.com>
2025-11-14 04:25:38 +0900

    refactor : 상담 답변내용 알림 목록 반환에 추가

--- src/main/java/org/likelion/_thon/silver_navi/domain/consult/repository/ConsultReplyRepository.java
+++ src/main/java/org/likelion/_thon/silver_navi/domain/consult/repository/ConsultReplyRepository.java
@@ -4,9 +4,15 @@ import org.likelion._thon.silver_navi.domain.consult.entity.ConsultReply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ConsultReplyRepository extends JpaRepository<ConsultReply, Long> {
 
     boolean existsByConsult_Id(Long consultId);
     boolean existsByGeneralConsult_Id(Long generalConsultId);
+
+    Optional<ConsultReply> findByConsult_Id(Long consultId);
+
+    Optional<ConsultReply> findByGeneralConsult_Id(Long generalConsultId);
 }
